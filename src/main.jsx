44a662edf8a5b141069734b3e28796b58237af5f
fmontallana <f.montallana.dit.cvsu@gmail.com>
2023-03-11 11:24:35 +0800

    update disabled react dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,8 @@ import './index.css'
 
 const queryClient = new QueryClient()
 
-//acquired from ChatGPT
-//disable react devtools in production
-if (process.env.NODE_ENV === 'production' && typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
-  for (let [key, value] of Object.entries(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
-    window.__REACT_DEVTOOLS_GLOBAL_HOOK__[key] = typeof value == 'function' ? () => { } : null;
-  }
+if (process.env.NODE_ENV === 'production') {
+  console.log = console.warn = console.error = () => { };
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
