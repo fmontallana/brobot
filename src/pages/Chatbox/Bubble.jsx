e3e48f029e4ef797d0ae59efac89249ac97b98ef
fmontallana <f.montallana.dit.cvsu@gmail.com>
2023-03-15 11:17:02 +0800

    update bubble ui

--- src/pages/Chatbox/Bubble.jsx
+++ src/pages/Chatbox/Bubble.jsx
@@ -11,18 +11,17 @@ function Bubble({ role, message, time, children }) {
     if (!user) return null
 
     return (
-        <div className={`flex gap-2 w-full h-auto px-2 ${role === "user" ? "flex-row-reverse" : ""} `}>
+        <div className={`flex justify-start gap-2 w-full h-auto px-2 ${role === "user" ? "flex-row-reverse" : ""} `}>
             {/* profile picture */}
             <div className='flex-shrink-0 self-end'>
-                {role === "user" ? <img src={user?.photoURL || `https://ui-avatars.com/api/?name=${user?.displayName}`} alt="profile picture" className="rounded-full w-10 h-10 border border-secondary-focus" /> : <img src={robot} height={10} width={10} alt="profile picture" className="rounded-full w-10 h-10 border border-secondary-focus" />}
-
+                <img src={role === "user" ? user?.photoURL || `https://ui-avatars.com/api/?name=${user?.displayName}` : robot} alt="profile picture" className="rounded-full w-8 h-8 border border-secondary-focus" />
             </div>
             {/* chat message */}
-            <div className={`w-9/12 min-h-14 flex flex-col ${role === "user" ? "text-right items-end" : "items-start"}`}>
+            <div className={`flex-1 min-h-14 flex flex-col ${role === "user" ? "text-right items-end" : "items-start"}`}>
 
                 <div className={`${role === "user" ? "bg-primary-content" : "border border-primary-content"} text-slate-200 px-2 py-1 rounded grid place-items-center`}>{message || children}</div>
-
             </div>
+            <div className='w-8' />
         </div>
     )
 }
